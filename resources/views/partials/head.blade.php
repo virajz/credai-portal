d9bd89c80dc7438147e6ae6b0f55214b8d915e16
Viraj Zaveri <44199181+virajz@users.noreply.github.com>
2025-11-04 15:39:12 +0530

    Branding

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,9 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<!-- Use PNG favicon from public/ -->
+<link rel="icon" href="/favicon.png" type="image/png">
+<link rel="apple-touch-icon" href="/favicon.png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
